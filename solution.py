ad70794f055c9ef8d111e5cc66fa08789f58532b
Mindgain <98777323+Mindgain@users.noreply.github.com>
2022-02-05 11:49:35 +0100

    Add files via upload

--- solution.py
+++ solution.py
@@ -9,11 +9,11 @@ def welcome_assignment_answers(question):
     if question =="In slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer ="mtls"
     elif question == "Are encoding and encryption the same? - Yes/No?":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No?":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question =="Is a hashed message supposed to be un-hashed? - Yes/No":
         answer="No"
     elif question =="What is the MD5 hashing value to the following message: 'NYU Computer Networking' ":
@@ -21,7 +21,7 @@ def welcome_assignment_answers(question):
     elif question =="Is MD5 a secured hashing algorithm? - Yes/No":
         answer="No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer=5
+        answer=4
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer=4
     return(answer)
