441575fc28fc5e31c4b2dfea9ddaff6526fc3cf8
Mindgain <98777323+Mindgain@users.noreply.github.com>
2022-02-05 10:46:23 +0100

    Add files via upload

--- solution.py
+++ solution.py
@@ -7,24 +7,22 @@ def welcome_assignment_answers(question):
     #Another way to implement is using a "case" statements similar to C.
     if question =="In slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer ="mtls"
-    elif question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+    elif question == "Are encoding and encryption the same? - Yes/No?":
+        answer = "Yes"
+    elif question == "Is it possible to decrypt a message without a key?":
         answer = "Yes"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
-    elif question =="Is a hashed message supposed to be un-hashed? - Yes/No ":
-        answer="No"
+    elif question =="Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer="Yes"
     elif question =="What is the MD5 hashing value to the following message: 'NYU Computer Networking' ":
         answer="65fc2f78620c22cc1f483c7d7201b592"
     elif question =="Is MD5 a secured hashing algorithm? - Yes/No":
         answer="No"
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to?":
-        answer= "7"
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to?":
-        answer="4"
-    else:
-        answer="Please ask a question"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+        answer= 7
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+        answer= 4
     return(answer)
 # Complete all the questions.
 
