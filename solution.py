5a4872f0e94f0f507ec85b7bb1c420a7128a13c2
Mindgain <98777323+Mindgain@users.noreply.github.com>
2022-02-05 13:40:44 +0100

    Add files via upload

--- solution.py
+++ solution.py
@@ -52,9 +52,9 @@
 def welcome_assignment_answers(question):
     switcher ={
       "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?" : "mtls",
-      "Are encoding and encryption the same? - Yes/No" :"Yes",
+      "Are encoding and encryption the same? - Yes/No" :"No",
       "Is it possible to decrypt a message without a key? - Yes/No": "No",
-      "Is it possible to decode a message without a key? - Yes/No":"No",
+      "Is it possible to decode a message without a key? - Yes/No":"Yes",
       "Is a hashed message supposed to be un-hashed? - Yes/No":"No",
       "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code": "42b76fe51778764973077a5a94056724",
       "Is MD5 a secured hashing algorithm? - Yes/No":"No",
