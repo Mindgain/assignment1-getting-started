71f59f7d07b04141226f4a32a05fc2d6225a3aeb
Mindgain <98777323+Mindgain@users.noreply.github.com>
2022-02-14 12:14:21 +0100

    Add files via upload

--- solution.py
+++ solution.py
@@ -24,7 +24,7 @@ def webServer(port=13331):
         outputdata = f.read()
         
         #Send one HTTP header line into socket.
-        connectionSocket.send("HTTP/1.1 200 OK\r\r\n")
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
         
 
         #Send the content of the requested file to the client
