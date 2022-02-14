dc6d39a4622279e80f2f4c9b78f1d598387f2e72
Mindgain <98777323+Mindgain@users.noreply.github.com>
2022-02-14 10:16:03 +0100

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,70 +1,53 @@
-### welcome_assignment_answers
-### Input - All eight questions given in the assignment.
-### Output - The right answer for the specific question.
-
-#def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    #answer = ""
-   # if question =="In slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-   #     answer ="mtls"
-  #  elif question == "Are encoding and encryption the same? - Yes/No?":
-    #    answer = "Yes"
-  #  elif question == "Is it possible to decrypt a message without a key? - Yes/No?":
-    #    answer = "Yes"
-  #  elif question == "Is it possible to decode a message without a key? - Yes/No":
-   #     answer = "Yes"
-  #  elif question =="Is a hashed message supposed to be un-hashed? - Yes/No":
-  #      answer="No"
-  #  elif question =="What is the MD5 hashing value to the following message: 'NYU Computer Networking' ":
-   #     answer="65fc2f78620c22cc1f483c7d7201b592"
-  #  elif question =="Is MD5 a secured hashing algorithm? - Yes/No":
-   #     answer="No"
-  #  elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-   #     answer=4
-  #  elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-   #     answer=3
-  #  else:
-   #     answer = "Whats the Question"
-  #  return(answer)
-
-# Complete all the questions.
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
 
 
-#if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-#    debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
-#    print(welcome_assignment_answers(debug_question))
+def webServer(port):
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  #Prepare a server socket
+  host = "127.0.0.1"
+  port = 4001
+  serverSocket.bind((host, port))
+  serverSocket.listen(5)
 
-###Questions:
-###"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
-###"Are encoding and encryption the same? - Yes/No"
-###"Is it possible to decrypt a message without a key? - Yes/No"
-###"Is it possible to decode a message without a key? - Yes/No"
-###"Is a hashed message supposed to be un-hashed? - Yes/No"
-###"What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
-###"Is MD5 a secured hashing algorithm? - Yes/No"
-###"What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
-###"What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
+  while True:
+    #Establish the connection
+    #print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
 
+      try:
+        message = connectionSocket.recv(1024)
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        
+        #Send one HTTP header line into socket.
+        connectionSocket.send('\nHTTP/1. x 200 OK\n')
+        
 
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+          connectionSocket.send(outputdata[i].encode())
 
-def welcome_assignment_answers(question):
-    switcher ={
-      "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?" : "mtls",
-      "Are encoding and encryption the same? - Yes/No" :"No",
-      "Is it possible to decrypt a message without a key? - Yes/No": "No",
-      "Is it possible to decode a message without a key? - Yes/No":"Yes",
-      "Is a hashed message supposed to be un-hashed? - Yes/No":"No",
-      "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code": "42b76fe51778764973077a5a94056724",
-      "Is MD5 a secured hashing algorithm? - Yes/No":"No",
-      "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":5,
-      "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":4,
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+      except IOError:
+        # Send response message for file not found (404)
+        connectionSocket.send('\n404 File Not Found\n')
 
-    }
-    return switcher.get(question, "nothing")    
+        #Close client socket
+        connectionSocket.close()
+
+        
+
+    except (ConnectionResetError, BrokenPipeError):
+      pass
+
+  serverSocket.close()
+  sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-   question = "Is MD5 a secured hashing algorithm? - Yes/No"
-   print(welcome_assignment_answers(question))
+  webServer(4001)
