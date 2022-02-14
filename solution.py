7d2937a98c76186b664583c2723c7b6694270651
Mindgain <98777323+Mindgain@users.noreply.github.com>
2022-02-14 16:08:17 +0100

    Add files via upload

--- solution.py
+++ solution.py
@@ -35,7 +35,8 @@ def webServer(port=13331):
         connectionSocket.close()
       except IOError:
         # Send response message for file not found (404)
-        connectionSocket.send('\n404 File Not Found\n')
+        connectionSocket.send(bytes('HTTP/1.1 File Not Found\r\n\r\n','UTF-8'))
+        #connectionSocket.send('\n404 File Not Found\n')
 
         #Close client socket
         connectionSocket.close()
